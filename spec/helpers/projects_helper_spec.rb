e881da53eda076cc0dfcdbdaccb285729fe13942
Dustin Mays <dustin.mays@uky.edu>
2012-12-18 14:15:35 -0500

    committing intitial specs for test suite

---
+++ spec/helpers/projects_helper_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+describe ProjectsHelper do
+  describe "#calculate_bayesian_rating" do
+    it "correctly determines a project's rating using Bayes' Theorem" do
+
+    end
+  end
+end
---
+++ spec/models/project_spec.rb
@@ -0,0 +1,25 @@
+#require 'models/project'
+require 'spec_helper'
+
+describe Project do
+  describe ".recent" do
+    it "includes the 10 newest projects" do
+
+    end
+
+    it "accepts projects with the minimum required data" do
+
+    end
+    it "does not accept projects without the minimum required data" do
+
+    end
+
+    it "does not allow project upvotes to be negative" do
+
+    end
+
+    it "does not allow project downvotes to be negative" do
+
+    end
+  end
+end
