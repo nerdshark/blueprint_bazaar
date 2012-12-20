f3fc864f030fa7e17f6d92e65db862dd31be12a0
Dustin Mays <dustin.mays@uky.edu>
2012-12-20 17:02:13 -0500

    configured test generator according to http://everydayrails.com/2012/03/12/testing-series-intro.html

--- config/application.rb
+++ config/application.rb
@@ -66,7 +66,14 @@ module BlueprintBazaar
     config.assets.version = '1.0'
 
     config.generators do |g|
-      g.fixture_replacement :factory_girl
+      g.test_framework :rspec,
+                       :fixtures => true,
+                       :view_specs => false,
+                       :helper_specs => false,
+                       :routing_specs => false,
+                       :controller_specs => true,
+                       :request_specs => true
+      g.fixture_replacement :factory_girl, :dir => "spec/factories"
     end
 
   end
