a988ff6d18deddb2a81db601df32b2568d5c0481
Dustin Mays <dustin.mays@uky.edu>
2012-12-20 21:15:36 -0500

    added factory for the step model

--- spec/factories/project_factory.rb
+++ spec/factories/project_factory.rb
@@ -2,9 +2,14 @@ require 'factory_girl'
 require 'faker'
 
 FactoryGirl.define do
-  factory :project do | p |
+  factory :project do |p|
     p.title {Faker::Lorem.characters 50}
     p.description {Faker::Lorem.paragraph}
     p.creator {Faker::Internet.user_name}
+  end
+
+  factory :step do |s|
+    s.title {Faker::Lorem.characters 50}
+    s.body {Faker::Lorem.paragraph}
   end
 end
