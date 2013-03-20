0a66f0861102c78e78ef4d31bee4acee3043a2f1
Dustin Mays <dustin.mays@uky.edu>
2013-03-20 17:32:57 -0400

    committing initial implementation of bayesian ranking algorithm

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,2 +1,17 @@
 module ProjectsHelper
+  # @param [Project] project
+  # @param [Integer] minimum_votes
+  def calculate_bayesian_rating(project, minimum_votes)
+    total_votes = (project.upvotes + project.downvotes)
+    net_votes = project.upvotes - project.downvotes
+    average_vote = net_votes / total_votes
+    ((total_votes/minimum_votes) * average_vote) + ((minimum_votes/(total_votes+minimum_votes)) * 0.5)
+  end
+
+  def sort_projects_by_bayesian()
+    #@sorted_projects = Array.new
+    #projects.each do |project|
+
+    #end
+  end
 end
